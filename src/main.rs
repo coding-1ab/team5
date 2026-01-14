593c3559b912e10f952a272e8e282a8a9605b4b1
say-annyeong <86085202+say-annyeong@users.noreply.github.com>
2026-01-15 00:50:31 +0900

    MasterPW.rs -> master_password.rs

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
-mod ui;
-
 fn main() -> Result<(), eframe::Error> {
     let options = eframe::NativeOptions::default();
 
     eframe::run_native(
         "eframe example",
         options,
-        Box::new(|_cc| Ok(Box::new(ui::GraphicalUserInterface::default()))),
+        Box::new(|_cc| Ok(Box::new(team5::ui::GraphicalUserInterface::default()))),
     )
 }
