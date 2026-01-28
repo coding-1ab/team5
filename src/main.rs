5ed47433d17913b811c4a14ca859f6a77f3af711
tmvkrpxl0 <tmvkrpxl0@proton.me>
2026-01-28 15:35:56 +0900

    ui 다시 활성화

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,17 @@ use single_instance::SingleInstance;
 
 fn main() -> Result<(), eframe::Error> {
     let instance = SingleInstance::new("team-5").unwrap();
-    if core::mem::size_of::<usize>() != 64 { error!("Unsupported Architecture") };
+    if size_of::<usize>() != 64 { error!("Unsupported Architecture") };
     
     if !instance.is_single() {
         // error!("This instance is not a single.");
         // return Ok(());
     }
-    let _options = eframe::NativeOptions::default();
-    Ok(())
+    let options = eframe::NativeOptions::default();
 
-    /*eframe::run_native(
+    eframe::run_native(
         "eframe example",
         options,
         Box::new(|_cc| Ok(Box::new(team5::ui::graphical_user_interface::GraphicalUserInterface::default()))),
-    )*/
+    )
 }
