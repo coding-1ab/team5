963de71ef85e6583069cbd1f2e81890d2d122fae
dkrgb <2048dk@gmail.com>
2026-02-05 03:14:39 +0900

    Unknown

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,18 @@
+extern crate core;
+
+use core::panic::const_assert;
+use single_instance::SingleInstance;
+
 fn main() -> std::io::Result<()> {
+    let instance = SingleInstance::new("team5");
+    if !instance.unwrap().is_single() {
+        return Ok(())
+    }
+
+    let s = core::mem::size_of::<usize>();
+    let mut arr1 = [0u8];
+    arr1[s-8] = 1;
+
 
     Ok(())
 }
