e935e2d8992c84f643b22a59dc3a7d3e4f5a73d8
say-annyeong <86085202+say-annyeong@users.noreply.github.com>
2026-02-05 22:58:52 +0900

    mod.rs -> lib.rs

---
+++ ui/src/lib.rs
@@ -0,0 +1,28 @@
+// todo
+// 사이트 이름 + 계정 정보 주면 저장
+// 사이트 이름 주면 계정 정보 불러오기
+
+// 흠 뭐부터 하지
+
+use std::marker::PhantomData;
+use engine::data_base::DB;
+
+#[cfg(feature = "gui")]
+pub mod graphical_user_interface;
+
+struct CharacterUserInterface {
+    db: DB
+}
+
+impl CharacterUserInterface {
+
+}
+
+struct UserInterface {
+    #[cfg(feature = "gui")]
+    graphical_user_interface: graphical_user_interface::GraphicalUserInterface,
+    #[cfg(feature = "cui")]
+    character_user_interface: CharacterUserInterface,
+    //_phantom_data: PhantomData<()>
+}
+
