ed3a435e39bc01497b46c3112060d71febc7ce99
tmvkrpxl0 <tmvkrpxl0@proton.me>
2026-03-28 00:58:19 +0900

    다른 폰트 제거

--- ui/src/main.rs
+++ ui/src/main.rs
@@ -41,17 +41,6 @@ fn init_fonts(cc: &CreationContext) {
         egui::FontData,
         epaint::text::{FontPriority, InsertFontFamily, FontInsert}
     };
-    let malgun_gothic_font_file_contents = fs::read(r"C:\Windows\Fonts\malgun.ttf").unwrap_or_else(|error| {
-        eprintln!("Error loading malgun gothic {}", error);
-        Vec::new()
-    });
-    let malgun_gothic_insert_font_family = InsertFontFamily {
-        family: egui::FontFamily::Proportional,
-        priority: if malgun_gothic_font_file_contents.is_empty() { FontPriority::Lowest } else { FontPriority::Highest },
-    };
-    let malgun_gothic_font_data = FontData::from_owned(malgun_gothic_font_file_contents);
-    let malgun_gothic_font = FontInsert::new("malgun_gothic", malgun_gothic_font_data, vec![malgun_gothic_insert_font_family]);
-    cc.egui_ctx.add_font(malgun_gothic_font);
     let nanum_gothic_font_data = FontData::from_static(include_bytes!("../NanumGothic.ttf"));
     let nanum_gothic_insert_font_family = InsertFontFamily {
         family: egui::FontFamily::Proportional,
@@ -59,15 +48,4 @@ fn init_fonts(cc: &CreationContext) {
     };
     let nanum_gothic_font = FontInsert::new("nanum_gothic", nanum_gothic_font_data, vec![nanum_gothic_insert_font_family]);
     cc.egui_ctx.add_font(nanum_gothic_font);
-    let emoji_font_file_contents = fs::read(r"C:\Windows\Fonts\seguiemj.ttf").unwrap_or_else(|error| {
-        eprintln!("Error loading emoji_font: {:?}", error);
-        Vec::new()
-    });
-    let emoji_font_data = FontData::from_owned(emoji_font_file_contents);
-    let emoji_insert_font_family = InsertFontFamily {
-        family: egui::FontFamily::Proportional,
-        priority: FontPriority::Lowest,
-    };
-    let emoji_font = FontInsert::new("windows_emoji", emoji_font_data, vec![emoji_insert_font_family]);
-    cc.egui_ctx.add_font(emoji_font);
 }
