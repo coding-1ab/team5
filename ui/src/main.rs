41770a14afa259a4e46536d6443887bb509d63b7
say-annyeong <86085202+say-annyeong@users.noreply.github.com>
2026-04-19 04:56:24 +0900

    feature수정

--- ui/src/main.rs
+++ ui/src/main.rs
@@ -44,7 +44,9 @@ fn main() {
             let center = get_monitor_center(get_hwnd(cc.raw_window_handle().unwrap()).unwrap()).unwrap();
             let mut graphical_user_interface = GraphicalUserInterface::default();
             #[cfg(feature = "windows")]
-            graphical_user_interface.center = center.into();
+            {
+                graphical_user_interface.center = center.into();
+            }
             cc.egui_ctx.send_viewport_cmd(ViewportCommand::Visible(false));
             //cc.egui_ctx.send_viewport_cmd(ViewportCommand::Visible(false)); // 있으니까 입력이 안되는데?
             init_fonts(cc);
